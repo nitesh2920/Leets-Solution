b970aaac65ae683bb551b2453dfa7ffbbfd09430
Nitesh Tiwari <89625680+nitesh2920@users.noreply.github.com>
2025-02-07 01:14:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (62.62%)

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int fib(int n) {
+
+        if(n<=1){
+            return n;
+        }
+        int prev1=0;
+        int prev2=1;
+
+
+        for(int i=2;i<=n;i++){
+            int curr=prev2+prev1;
+            prev1=prev2;
+            prev2=curr;
+
+        }
+        
+        return prev2;
+    }
+};
